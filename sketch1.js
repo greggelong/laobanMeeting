94bfbbf6254f9a02009340b81e26a59d0aba64f7
Greg K <gkreisman_cep@qq.com>
2025-03-02 10:50:24 +0800

    add bone

--- sketch1.js
+++ sketch1.js
@@ -14,7 +14,7 @@ let bkw;
 let talking = false;
 let cbard;
 function preload() {
-  story = loadStrings("meeting.txt");
+  story = loadStrings("bone.txt");
   //story = loadStrings("site.txt");
 }
 
