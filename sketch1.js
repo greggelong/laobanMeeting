0a0b9341c2bc277cf0f1934e9e496ea1423cdd7b
Greg K <gkreisman_cep@qq.com>
2025-06-25 15:03:46 +0800

    add for food

--- sketch1.js
+++ sketch1.js
@@ -14,7 +14,7 @@ let bkw;
 let talking = false;
 let cbard;
 function preload() {
-  story = loadStrings("poemsT2.txt");
+  story = loadStrings("laobanFood.txt");
   //story = loadStrings("site.txt");
 }
 
