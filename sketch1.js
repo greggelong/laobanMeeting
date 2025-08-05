5b5abf083bba1b34f0a89afeebbb0d4a1831d80c
Greg K <gkreisman_cep@qq.com>
2025-08-05 09:38:12 +0200

    change title

--- sketch1.js
+++ sketch1.js
@@ -14,7 +14,7 @@ let bkw;
 let talking = false;
 let cbard;
 function preload() {
-  story = loadStrings("laobanFood.txt");
+  story = loadStrings("laobanfood.txt");
   //story = loadStrings("site.txt");
 }
 
