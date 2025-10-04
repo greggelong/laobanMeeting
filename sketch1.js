74390d1e52ff453bcbd562a4a1530284fd4136fe
Greg K <gkreisman_cep@qq.com>
2025-10-04 21:09:33 +0800

    changes for park

--- sketch1.js
+++ sketch1.js
@@ -14,7 +14,7 @@ let bkw;
 let talking = false;
 let cbard;
 function preload() {
-  story = loadStrings("hainan2.txt");
+  story = loadStrings("park.txt");
   //story = loadStrings("site.txt");
 }
 
--- sketch2.js
+++ sketch2.js
@@ -13,7 +13,7 @@ let talking = false;
 let cbard;
 function preload() {
   //story = loadStrings("grid1.txt");
-  story = loadStrings("mall.txt");
+  story = loadStrings("park.txt");
 }
 
 function setup() {
